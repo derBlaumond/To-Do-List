f35731f2e55e4d54f5f3f7cdcd363f10b69b800f
derBlaumond <parksmkim@gmail.com>
2023-03-24 05:00:57 +0100

    making CSS, modify some codes

--- js/greetings.js
+++ js/greetings.js
@@ -4,26 +4,36 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
- 
-function onLoginSubmit(event){
+const time = new Date();
+const hours = time.getHours();
+
+function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY,loginInput.value);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
   paintGreetings();
 }
-function paintGreetings(){
+
+function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
-  greeting.innerText = `Hello ${username}!!`;
+  if (hours > 6 && hours <= 12) {
+    greeting.innerText = `Good Morning, ${username}!!`;
+  } else if (hours > 12 && hours <= 18) {
+    greeting.innerText = `Good Afternoon, ${username}!!`;
+  } else if (hours > 18 && hours <= 24) {
+    greeting.innerText = `Good Evening, ${username}!!`;
+  } else if (hours <= 6) {
+    greeting.innerText = `Good Night, ${username}!!`;
+  }
+
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
+if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-}else{
+} else {
   paintGreetings();
 }
- 
--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,21 @@
 const API_KEY = "a957866f9916e0efc55bcaa034d6dea0";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const icon = document.querySelector("#weather-icon");
 
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
     .then((Response) => Response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-      weather.innerText = `${data.weather[0].main}, ${data.main.temp}`;
       city.innerText = data.name;
+      const weatherData = data.weather[0].main;
+      const tempData = data.main.temp;
+      weather.innerText = `${weatherData}, ${tempData}'C `;
+      icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     });
 }
 function onGeoFail() {
